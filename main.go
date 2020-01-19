a476ec969ad9b33d5846992a9895846f5b7ccf74
Kiyumars <maessoumi@fitanalytics.com>
2020-01-19 11:28:10 +0100

    so muuuuuch progress

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"html/template"
+	"net/http"
 )
+
+var tmpl = template.Must(template.ParseGlob("templates/*"))
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	err := tmpl.ExecuteTemplate(w, "Hello", nil)
+	if err != nil {
+		log.Printf("Unexepected err: %v", err)
+	}
+}
 
 func main() {
 	fmt.Println("Hello Word")
+	http.HandleFunc("/", helloHandler)
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
